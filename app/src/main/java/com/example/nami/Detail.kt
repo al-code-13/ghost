bba2ab62437835890a8e347a6ab61860658d92e3
Alejo <carlos.velam@uniagustiniana.edu.co>
2020-05-11 12:35:44 -0500

    Detail + dashboard
    Falta lo tuio edy :)

---
+++ app/src/main/java/com/example/nami/Detail.kt
@@ -0,0 +1,12 @@
+package com.example.nami
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Detail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
