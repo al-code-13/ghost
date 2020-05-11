8ee65aa62fcd3caa075ccb2e8efffbe96f810a31
Luis Miguel <luismiguelavlarez46@gmail.com>
2020-05-11 12:46:16 -0500

    Errores Errores y mas errores

---
+++ app/src/main/java/com/example/nami/Login.kt
@@ -0,0 +1,16 @@
+package com.example.nami
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
---
+++ app/src/main/java/com/example/nami/helpPage.kt
@@ -0,0 +1,12 @@
+package com.example.nami
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class helpPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help_page)
+    }
+}
